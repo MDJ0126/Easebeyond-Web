15e4e6054d26320338eb28e132ecc17f084c5a5a
문동준 <ehdwns0126@gmail.com>
2026-07-17 21:25:16 +0900

    빌드

--- Build/ServiceWorker.js
+++ Build/ServiceWorker.js
@@ -41,12 +41,10 @@ self.addEventListener('fetch', function (e) {
       }
 
       let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (response) { return response; }
 
       response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })());
